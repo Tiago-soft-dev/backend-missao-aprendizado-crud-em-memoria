d72dd9aac96e20f8800e1ec4c85ea5f81cf5c090
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-07-13 22:37:21 -0300

    endpoint read by id

--- index.js
+++ index.js
@@ -9,5 +9,11 @@ app.get('/personagem', (req,res)=>{
     res.send(lista)
 })
 
+//endpoint read by id get
+app.get('/personagem/:id', (req,res)=>{
+    let id = req.params.id
+    res.send(lista[id - 1]);
+})
+
 
 app.listen(3000, ()=>console.log('servidor online'))
